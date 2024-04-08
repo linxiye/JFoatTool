3898d1512f5da74a323274895ab4af7231aca0bb
linxiye <448419145@qq.com>
2024-04-08 14:27:14 +0800

    janpatch pc tool

---
+++ mainwindow.h
@@ -0,0 +1,48 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QProcess>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void LogOut();
+    void Fota_Creat_Patch(QString patchName);
+private:
+    Ui::MainWindow *ui;
+
+    QProcess *process;
+};
+
+#define FOTA_FILE_SOURCE            0
+#define FOTA_FILE_TARGET            1
+#define FOTA_FILE_PATCH             2
+#define FOTA_FILE_PATCH_NAME        "diff.patch"
+#define FOTA_FILE_PATCH_BIN_NAME    "diff.bin"
+
+struct fota_file_info
+{
+    quint32 file_size;
+    quint32 file_crc;
+};
+
+#endif // MAINWINDOW_H
